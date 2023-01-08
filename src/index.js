c2b2af13581897ce8977ebb543f52e2b4411938e
“Irina <irina.osipova2012@gmail.com>
2023-01-08 15:53:00 +0200

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    let i = 0;
+    let result = '';
+    if (n<0) {
+        n=n*(-1);
+    }
+    str = n.toString();
+ 
+    while (i < str.length) {
+      result = `${str[i]}${result}`;
+      i = i + 1;
+    }
+    return result;
 }
